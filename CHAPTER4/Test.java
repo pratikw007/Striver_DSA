36fb891ae5f388e385d3f48edf80f89abeed8453
pratikw007 <waghmarepratik287@gmail.com>
2024-02-03 20:38:05 +0530

    Main Branch

--- CHAPTER4/Test.java
+++ CHAPTER4/Test.java
@@ -1,12 +1,12 @@
-package CHAPTER4;
+// package CHAPTER4;
 
-public class Test {
-    public static void num(int i,int n) {
-        if (i> n) {
-             return;        
-        }
-        System.out.println(i);
-        num(i+1,n);  //Recursive call with incremented value of '
-     }
+// public class Test {
+//     public static void num(int i,int n) {
+//         if (i> n) {
+//              return;        
+//         }
+//         System.out.println(i);
+//         num(i+1,n);  //Recursive call with incremented value of '
+//      }
     
-}
+// }
